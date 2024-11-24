4314bead1faf24344ca980679a0646bf5f5e76d7
arnirohandi <164883142+arnirohandi@users.noreply.github.com>
2024-11-24 12:54:27 +0100

    Fix movie card

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -7,8 +7,8 @@ import "./movie-card.scss";
 
 export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100">
-      <Card.Img variant="top" src={movie.image} />
+    <Card className="h-100 w-100">
+      <Card.Img variant="top" className="img-fluid" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.director}</Card.Text>
