b389735027820f17a405ab5ba9e881e8bb5305f0
arnirohandi <164883142+arnirohandi@users.noreply.github.com>
2024-12-04 12:45:52 +0100

    Update NavBar and profile view

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="custom-navbar" bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" className="navbar-brand">
           myFlix
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -13,24 +13,24 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
           <Nav className="me-auto">
             {!user && (
               <>
-                <Nav.Link as={Link} to="/login">
-                  Login
+                <Nav.Link as={Link} to="/login" className="navbar-link">
+                  LOGIN
                 </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
+                <Nav.Link as={Link} to="/signup" className="navbar-link">
+                  SIGNUP
                 </Nav.Link>
               </>
             )}
             {user && (
               <>
-                <Nav.Link as={Link} to="/">
-                  Home
+                <Nav.Link as={Link} to="/" className="navbar-link">
+                  HOME
                 </Nav.Link>
-                <Nav.Link as={Link} to="/profile">
-                  Profile
+                <Nav.Link as={Link} to="/profile" className="navbar-link">
+                  PROFILE
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>
-                  Logout
+                <Nav.Link onClick={onLoggedOut} className="navbar-link">
+                  LOGOUT
                 </Nav.Link>
               </>
             )}
